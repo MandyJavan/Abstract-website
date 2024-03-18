7e6e669dfb35f4d06b05a9657f8891423cadce89
MandyJavan <javanbakhtmeh@gmail.com>
2024-03-19 00:10:53 +0330

    All Done! hidden search has been placed in header!

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let hamburger = document.getElementById("hamburger");
 let btnToList = document.querySelector("#btnToList");
+let searchIcon = document.querySelector("#searchIcon")
+let header = document.querySelector("#header")
+let headerLeft = document.getElementById("headerLeft")
+let headerRight = document.getElementById("headerRight")
+let hiddenSearch = document.querySelector("#hiddenSearch")
+let closeHidden = document.querySelector("#closeHidden")
 let hamburger_isClicked = false;
+let search_isClicked = false;
 
 hamburger.addEventListener("click", ()=> {
     hamburger_isClicked = !hamburger_isClicked;
@@ -12,4 +19,26 @@ hamburger.addEventListener("click", ()=> {
         hamburger.classList.remove("bx-x")
         btnToList.style.display = "none"
     }
+})
+
+searchIcon.addEventListener( 'click', ()=> {
+    // search_isClicked = !search_isClicked;
+    // if(search_isClicked){
+    //     headerLeft.style.display = "none"
+    //     headerRight.style.display = "none"
+    //     hiddenSearch.style.display = "flex"
+    //     header.classList.toggle('bg-white')
+    // }
+    headerLeft.style.display = "none"
+    headerRight.style.display = "none"
+    hiddenSearch.style.display = "flex"
+    header.classList.toggle('bg-white')
+
+}) 
+
+closeHidden.addEventListener("click", () => {
+        headerLeft.style.display = "flex";
+        headerRight.style.display = "flex";
+        hiddenSearch.style.display = "none"
+        header.classList.toggle('bg-white')
 })
